9164524ea118e0d1af515e7f15358263ec6f60e1
Rohan Ray <rohanray@unc.edu>
2023-04-28 15:22:53 -0400

    bug fixing

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -5,32 +5,37 @@ import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
 
+const rule = `Rules for Rock Paper Scissors:
+- Scissors CUTS Paper
+- Paper COVERS Rock
+- Rock CRUSHES Scissors`;
+const hel = `Usage: node-rps [SHOT]
+Play Rock Paper Scissors (RPS)
+
+  -h, --help      display this help message and exit
+  -r, --rules     display the rules and exit
+
+Examples:
+  node-rps        Return JSON with single player RPS result.
+                  e.g. {"player":"rock"}
+  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}
+ `;
+
 if (args.r || args.rules){
-    console.log( `Rules for Rock Paper Scissors:
-   - Scissors CUTS Paper
-   - Paper COVERS Rock
-   - Rock CRUSHES Scissors`);
+    console.log(rule);
     process.exit(0);
 }
 
 if (args.h || args.help){
-    console.log(`Usage: node-rps [SHOT]
-   Play Rock Paper Scissors (RPS)
-   
-     -h, --help      display this help message and exit
-     -r, --rules     display the rules and exit
-   
-   Examples:
-     node-rps        Return JSON with single player RPS result.
-                     e.g. {"player":"rock"}
-     node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                     e.g {"player":"rock","opponent":"scissors","result":"win"}
-    `);
+    console.log(hel);
     process.exit(0);
 }
 
 if (args._.length > 1){
     console.error("Argument is out of range");
+    console.log(hel);
+    console.log(rule);
     process.exit(0);
 } else if (args._.length === 0) {
     console.log(JSON.stringify(rpsls.rpsDef()))
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -6,39 +6,45 @@ import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
 
-if (args.r || args.rules){
-    console.log(`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:\n
-    - Scissors CUTS Paper
-    - Paper COVERS Rock
-    - Rock SMOOSHES Lizard
-    - Lizard POISONS Spock
-    - Spock SMASHES Scissors
-    - Scissors DECAPITATES Lizard
-    - Lizard EATS Paper
-    - Paper DISPROVES Spock
-    - Spock VAPORIZES Rock
-    - Rock CRUSHES Scissors`);
-    process.exit(0);
+const hel = `Usage: node-rpsls [SHOT]
+Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
 
+  -h, --help        display this help message and exit
+  -r, --rules       display the rules and exit
+
+Examples:
+  node-rpsls        Return JSON with single player RPSLS result.
+                    e.g. {"player":"rock"}
+  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`;
+
+
+const rule = `Rules for the Lizard-Spock Expansion of Rock Paper Scissors:\n
+- Scissors CUTS Paper
+- Paper COVERS Rock
+- Rock SMOOSHES Lizard
+- Lizard POISONS Spock
+- Spock SMASHES Scissors
+- Scissors DECAPITATES Lizard
+- Lizard EATS Paper
+- Paper DISPROVES Spock
+- Spock VAPORIZES Rock
+- Rock CRUSHES Scissors`;
+
+if (args.r || args.rules){
+    console.log(rule);
+    process.exit(0);
 }
 
 if (args.h || args.help){
-    console.log(`Usage: node-rpsls [SHOT]
-    Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
-    
-      -h, --help        display this help message and exit
-      -r, --rules       display the rules and exit
-    
-    Examples:
-      node-rpsls        Return JSON with single player RPSLS result.
-                        e.g. {"player":"rock"}
-      node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-                        e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
+    console.log(hel);
     process.exit(0);
 }
 
 if (args._.length > 1){
     console.error("Arguments out of range");
+    console.log(hel);
+    console.log(rule);
     process.exit(0);
 } else if (args._.length === 0) {
     console.log(JSON.stringify(rpsls.rpslsDef()))
