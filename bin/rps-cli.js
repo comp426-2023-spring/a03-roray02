8e974095a33b6f51ecc7068f166eb2106e2a5e7b
Rohan Ray <rohanray@unc.edu>
2023-04-28 13:08:23 -0400

    rpsls defined

---
+++ bin/rps-cli.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import rps from "../lib/rps.js";
+import minimist from "minimist"; 
+
---
+++ bin/rpsls-cli.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+import rpsls from "../lib/rpsls.js";
+import minimist from "minimist"; 
